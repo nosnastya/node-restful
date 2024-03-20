8f0d5572d80ca4a65120cd6fafc771817544d288
Anastasiia Nos <nosnastya@gmail.com>
2024-03-20 12:32:56 +0400

    add initial test for signup

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -1,5 +1,32 @@
 const request = require('supertest');
+const app = require('../app');
 
-it('returns 200 when sign up request is valid', () => {
-  
-})
+describe('User registration', () => {
+  it('returns 200 when sign up request is valid', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@gmail.com',
+        password: 'P4ssword',
+      })
+      .then((response) => {
+        expect(response.status).toBe(200);
+        done();
+      });
+  });
+
+  it('returns success message if signup request is valid', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@gmail.com',
+        password: 'P4ssword',
+      })
+      .then((response) => {
+        expect(response.body.message).toBe('User created');
+        done();
+      });
+  });
+});
--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3002, () => console.log('app is running'));
+app.post('/api/1.0/users', (req, res) => {
+  return res.send({ message: 'User created' });
+});
+
+module.exports = app;
